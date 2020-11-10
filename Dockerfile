9285a8fc1f4e738dd44d695396889ec1189289e0
thatscotdatasci <alan@thatscottishdatascientist.com>
2020-11-10 13:41:19 +0000

    Updating python and library versions; using new streamlit page config functionality

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7
+FROM python:3.8
 
 # Create dir for the app
 WORKDIR /streamlit_app/
--- app/TSDSStreamlitExample.py
+++ app/TSDSStreamlitExample.py
@@ -13,10 +13,18 @@ from app.navigation_manager.navigation_manager import NavigationManager
 
 TSDS_ICON_URL = "https://raw.githubusercontent.com/thatscotdatasci/thatscotdatasci.github.io/master/assets/icons/tsds.ico"
 
-
-# Display the TSDS logo in the sidebar
+# Get the TSDS icon
 tsds_icon_data = requests.get(TSDS_ICON_URL)
 tsds_icon = Image.open(BytesIO(tsds_icon_data.content))
+
+# Set the page configuration
+st.set_page_config(
+    page_title="TSDS Streamlit Example",
+    page_icon=tsds_icon,
+    initial_sidebar_state="expanded"
+)
+
+# Display the TSDS logo in the sidebar
 st.sidebar.image(tsds_icon)
 
 # App title in sidebar
